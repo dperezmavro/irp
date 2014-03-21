f7c1386edeb8221b1d85f046487bbabfe8e216bf
dperezmaavro <dpm3g10@ecs.soton.ac.uk>
2014-03-21 13:36:39 +0000

    greatness

--- report/commands.bash
+++ report/commands.bash
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/bin/bash -- 
 
-if [ "$1" = "-c" -o "$1" = "--clean" ]; then
+if [ $# -lt 1 ]; then
+    echo "Need to specify some options..."
+    echo "    -c , --clean : remove temporary files"
+elif [ "$1" = "-c" -o "$1" = "--clean" ]; then
     echo "Cleaning temporary files..."
     rm *.log *.aux *.out *.gz *.bbl *.blg
 fi
