eb8b6bbddd10085753dc128710a3b7e6626d259c
dperezmavro <dpm3g10@ecs.soton.ac.uk>
2014-03-27 12:40:28 +0000

    wrote draft of abstract

--- report/commands.bash
+++ report/commands.bash
@@ -5,7 +5,7 @@ if [ $# -lt 1 ]; then
     echo "    -c , --clean : remove temporary files"
 elif [ "$1" = "-c" -o "$1" = "--clean" ]; then
     echo "Cleaning temporary files..."
-    rm *.log *.aux *.out *.gz *.bbl *.blg
+    rm *.log *.aux *.out *.ps *.dvi *.gz *.bbl *.blg
 elif [ "$1" = "-s" -o "$1" = "--save" ]; then
     git commit -am "$2"
     git push origin master
